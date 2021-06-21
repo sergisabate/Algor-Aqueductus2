30fb3bfb47c9cd998383d065d318c2e4b295ac31
sergisabate <sergisabatevila@gmail.com>
2021-06-21 21:41:52 +0200

    greedy

--- greedy.py
+++ greedy.py
@@ -1,9 +1,10 @@
 import math
 import sys
+import operator
 
 
 def readfile():
-    filename = "secret-01.in"
+    filename = sys.argv[1]
     variables = []
     for line in open(filename):
         variables.append([int(i) for i in line.strip().split(' ')])
@@ -16,31 +17,13 @@ def readfile():
     return variables[0]
 
 
-def cost(height, x):
-    return alpha * height + beta * x
+def cost(data):
+    return alpha * data[0] + beta * data[1]
 
 
 def impossible():
     print("impossible")
     exit()
-
-
-def permutation(lst, points_perm):
-    if len(lst) == 0:
-        return []
-    if len(lst) == 2:
-        return [lst]
-    l = []
-    for i in range(len(lst)):
-        m = lst[i]
-        remLst = lst[:i] + lst[i + 1:]
-        s = permutation(remLst, points_perm)
-        for p in s:
-            if m < p[0] and p.count(points_perm[-1]):
-                l.append([m] + p)
-            if not l.count(p) and p.count(points_perm[0]) and p.count(points_perm[-1]):
-                l.append(p)
-    return l
 
 
 def possible(points_pos, dist_x):
@@ -67,9 +50,6 @@ def sum_values(result, height, width):
 
 
 def optimum_aqueduct(ground):
-    ind = list(range(len(ground)))
-    comb = permutation(ind, ind)
-    result = [0, 0]
     solution = greedy(ground, [0, 0, 0])
     if not solution:
         impossible()
@@ -80,30 +60,29 @@ def greedy(points_greed, result):
     if len(points_greed) == 0:
         return [0, 0, 0]
     now = points_greed[0]
-    error = False
-    s = [0, 0, 0]
     minimum = [0, 0, 0]
     for i in list(range(1, len(points_greed))):
+        s = [0, 0, 0]
+        error = False
         after = points_greed[i]
         dist_x = after[0] - now[0]
         if not possible([now, after], dist_x):
             error = True
         elif len(points_greed) == 2:
             s = sum_values(s, h * 2 - now[1] - after[1], dist_x ** 2)
-            s[2] = i
+            return s
         else:
             s = sum_values(s, h - now[1], dist_x ** 2)
             s[2] = i
         if i == 1 and not error:
             minimum = s
-        elif cost(minimum[0], minimum[1]) > cost(s[0], s[1]) and not error:
+        elif cost(minimum) > cost(s) and not error:
             minimum = s
     if error and minimum == [0, 0, 0]:
         impossible()
-    greedy(points_greed[result[2]:], result + minimum)
-    return result
+    return greedy(points_greed[minimum[2]:], list(map(operator.add, result, minimum)))
 
 
 if __name__ == "__main__":
     n, h, alpha, beta, points = readfile()
-    print(optimum_aqueduct(points))
+    print(cost(optimum_aqueduct(points)))
